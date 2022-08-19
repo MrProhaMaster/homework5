16249315d7fff44794f6f627627eccea5873a47c
MrProha <mrprohagames@gmail.com>
2022-08-19 12:08:16 +0300

    task3 - done

--- homework2.py
+++ homework2.py
@@ -9,4 +9,18 @@ for file in l:
     with open(loc, encoding='utf-8') as f:
         txt = f.readlines()
         slovar.setdefault(loc, len(txt))
-print(slovar)
+slovar_sorted = {}
+sorted_lens = sorted(slovar.values())
+for i in sorted_lens:
+    for n in slovar:
+        if slovar[n] == i:
+            slovar_sorted.setdefault(n, slovar[n])
+            break
+print(slovar_sorted)
+with open('result_txt.txt', 'w', encoding='utf-8') as f:
+    for i in slovar_sorted:
+        with open(i, encoding='utf-8') as f1:
+            txt = f1.read()
+        f.writelines(i+'\n')
+        f.writelines(str(slovar_sorted[i])+'\n')
+        f.writelines(txt+'\n')
